48db7e230013aa5f9dba007376a2ca0588c33043
Franco Cardenas <gustavo260123@gmail.com>
2022-06-07 15:31:38 -0700

    quotec

---
+++ hello_world/101-quote.c
@@ -0,0 +1,11 @@
+{#incluir < stdio.h >
+/**
+ *main - Punto de inicio
+ *
+ *Return: Retornar "1" (accion)
+ **/
+int main(void)
+{
+write(2, ""y esa obra de arte es útil" - Dora Korpar, 2015-10-19\n" 61);
+return (1);
+}}

40629956ed1186afd4176a3df2ee71d038117f77
Franco Cardenas <gustavo260123@gmail.com>
2022-06-07 15:49:37 -0700

    quotec

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#incluir < stdio.h >
+#include <stdio.h>
 /**
  *main - Punto de inicio
  *
@@ -6,6 +6,6 @@
  **/
 int main(void)
 {
-write(2, ""and that piece of art is useful" - Dora Korpar, 2015-10-19\n" 61);
+write(2, ""and that piece of art is useful" - Dora Korpar, 2015-10-19 \n", 61);
 return (1);
 }

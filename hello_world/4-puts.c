3dea44a6e69cea02580fb31788f40501cbdf4658
Franco Cardenas <gustavo260123@gmail.com>
2022-06-06 18:57:40 -0700

    putsc

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-/*no es necesario agregar muchas cosas ya que no tienen sentido en C*/
-int main(void)
-{
+/**
+ * main punto de inicio de ejecucion
+ *
+ * return para devolver un valor
+ */
+int main(void) {
 	puts("Programming is like building a multilingual puzzle ");
+	return (0);
 }

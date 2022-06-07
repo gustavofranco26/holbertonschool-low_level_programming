7b7109e0181b9a3d27b43a97975a956086e2f253
Franco Cardenas <gustavo260123@gmail.com>
2022-06-07 14:49:17 -0700

    sizec

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,10 +16,10 @@ long c;
 long long d;
 float e;
 
-printf("size of a char: %lu bytes(s)\n", sizeof(a));
-printf("size of an int: %lu bytes(s)\n", sizeof(b));
-printf("size of a long int: %lu bytes(s)\n", sizeof(c));
-printf("size of a long long int: %lu bytes(s)\n", sizeof(d));
-printf("size of a long double: %lu bytes(s)\n", sizeof(e));
+printf("Size of a char: %lu bytes(s)\n", sizeof(a));
+printf("Size of an int: %lu bytes(s)\n", sizeof(b));
+printf("Size of a long int: %lu bytes(s)\n", sizeof(c));
+printf("Size of a long long int: %lu bytes(s)\n", sizeof(d));
+printf("Size of a float : %lu bytes(s)\n", sizeof(e));
 return (0);
 }

0657d442df790c5698c860b39e0596d2c0b0592f
Franco Cardenas <gustavo260123@gmail.com>
2022-06-06 19:30:23 -0700

    printfc

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Punto de inicio
+ *
+ * Return: Retornar "0" (accion)
+*/
+
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
+}

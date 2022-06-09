7480aa66c7ebdca69e0eb022f58812cd3713a11e
Franco Cardenas <gustavo260123@gmail.com>
2022-06-09 01:03:02 -0700

    printtebahplareves

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Punto de inicio
+ *
+ *Return: Retornar "0" (accion)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 95; i--)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

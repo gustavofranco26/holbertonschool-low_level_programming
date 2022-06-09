e322a5c568e0315a69ecf010bd4cb068c46d675c
Franco Cardenas <gustavo260123@gmail.com>
2022-06-09 01:27:32 -0700

    printcomb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Punto de inicio
+ *
+ *Return: Retornar "0" (accion)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	if (i != 57)
+		putchar(44);
+	putchar(32);
+	putchar('\n');
+	}
+	return (0);
+}

c909dec4da753b1d09f68c18f3ab51252d57943d
Franco Cardenas <gustavo260123@gmail.com>
2022-06-09 00:37:14 -0700

    printnumbersc

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Punto de inicio
+ *
+ *Return: Retornar "0" (accion)
+ */
+int main(void)
+{
+
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}

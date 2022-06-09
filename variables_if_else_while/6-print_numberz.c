6d503f1d270843f18686aabb7b267af48fb63921
Franco Cardenas <gustavo260123@gmail.com>
2022-06-09 00:57:23 -0700

    printnumberzc6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 /**
  *main - Punto de inicio
@@ -8,12 +7,12 @@
 int main(void)
 {
 
-        int i;
+	int i;
 
-        for (i = '0'; i <= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
         {
-                putchar(i);
-        }
-        putchar('\n');
-        return (0);
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }

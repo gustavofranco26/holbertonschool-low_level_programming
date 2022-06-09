8cfc5e6a9e4e893293c0c1bd9a3f0e3176787f27
Franco Cardenas <gustavo260123@gmail.com>
2022-06-09 00:07:52 -0700

    printalphabetsc

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,8 +9,9 @@ int main(void)
 	int i;
 	/*ASCII- 97=a, 122=z*/
 	for (i = 97; i < 123; i++)
-		if (i != 113 && i != 101)
-		putchar(i);
+			putchar(i);
+	for (i = 65; i < 91; i++)
+			putchar(i);
 	return (0);
 	putchar('\n');
 }

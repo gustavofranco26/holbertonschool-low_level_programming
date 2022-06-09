2395f120efae6f3f1b4c7b7284cef9ed1bdd9ee6
Franco Cardenas <gustavo260123@gmail.com>
2022-06-09 10:37:55 -0700

    prints_putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ *Descripcion: "print _putchar"
+ *
+ * Return: Always "0".(Accion)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	_putchar('\n');
+	return (0);
+}

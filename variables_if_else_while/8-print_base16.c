8f78508486c536e8503a23f2d88bb5ae028fabc8
Franco Cardenas <gustavo260123@gmail.com>
2022-06-09 01:18:13 -0700

    printbase16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,8 +9,9 @@ int main(void)
 	int i;
 
 	for (i = 48; i <= 57; i++)
-	for (i = 65; i <= 70; i++)
-		putchar (i);
+		putchar(i);
+	for (i = 97; i <= 102; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }

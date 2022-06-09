8660b252e36659a5eb37dfeb2ff1edd7430e6916
Franco Cardenas <gustavo260123@gmail.com>
2022-06-09 01:16:27 -0700

    printbase16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Punto de inicio
+ *
+ *Return: Retornar "0" (accion)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	for (i = 65; i <= 70; i++)
+		putchar (i);
+	putchar('\n');
+	return (0);
+}

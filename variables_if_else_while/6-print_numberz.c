a663a08f443da025b0458f9860ff102e50e34bae
Franco Cardenas <gustavo260123@gmail.com>
2022-06-09 00:55:40 -0700

    printnumberzc6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+/**
+ *main - Punto de inicio
+ *
+ *Return: Retornar "0" (accion)
+ */
+int main(void)
+{
+
+        int i;
+
+        for (i = '0'; i <= '9'; i++)
+        {
+                putchar(i);
+        }
+        putchar('\n');
+        return (0);
+}

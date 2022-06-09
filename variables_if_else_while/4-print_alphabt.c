9a916e2092dd1bba0b16163b4526c718655e1a44
Franco Cardenas <gustavo260123@gmail.com>
2022-06-09 00:21:15 -0700

    printalphabet

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Punto de inicio
+ *
+ *Return: Retornar "0" (accion)
+ */
+int main(void)
+{
+
+	int i;
+	/*ASCII- 97=a, 122=z*/
+	for (i = 97; i < 123; i++)
+		if (i != 113 && i != 101)
+			putchar(i);
+	putchar('\n');
+	return (0);
+}

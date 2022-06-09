16c836be413f3e34588c47c4797dfddd6213c744
Franco Cardenas <gustavo260123@gmail.com>
2022-06-08 23:23:13 -0700

    printalphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,15 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 /**
  *main - Punto de inicio
  *
- *Return: Retornar "0" (accion = zero)
+ *Return: Retornar "0" (accion)
  */
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int i;
 
 	for (i = 0; i <= 25; i++)
 		putchar(97 + i);
+	return (i);
 }

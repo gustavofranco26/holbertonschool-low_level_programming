ebc9f69f79d19a818b7f4a1ed78f6f0b9eeb68f6
Franco Cardenas <gustavo260123@gmail.com>
2022-06-10 13:13:24 -0700

    printanynumber98c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,9 +11,9 @@ void print_to_98(int n)
 	int i;
 
 	for (i = n ; i < 98; i++)
-		printf("%i\n", i);
+		printf("%i, ", i);
 
 	for (i = n ; i > 98; i--)
-		printf("%i\n", i);
+		printf("%i, ", i);
 	printf("%i\n", i);
 }

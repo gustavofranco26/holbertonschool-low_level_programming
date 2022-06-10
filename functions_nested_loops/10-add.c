524eec43197bda7e28a31df5ccf632d696a99ad8
Franco Cardenas <gustavo260123@gmail.com>
2022-06-10 12:18:25 -0700

    addtwonumbersc

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Adition the a and b.
+ * @a: numero 1
+ * @b: numero 2
+ * Descripcion: adds two integers and returns the result.
+ * Return: suma de ambos numeros
+ */
+int add(int a, int b)
+{
+	int suma;
+	
+	suma = a + b;
+	return (suma);
+}

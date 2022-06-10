b47a17fe1da116734187d4b0216d796cd2d3fa4f
Franco Cardenas <gustavo260123@gmail.com>
2022-06-10 10:30:49 -0700

    print24hoursc

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,10 +12,10 @@ void jack_bauer(void)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			_putchar(H / 10) + '0');
-			_putchar(H % 10) + '0');
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
 			_putchar(':');
-			_putchar(m / 10) + '0)';
+			_putchar((m / 10) + '0)';
 			_putchar((m % 10) + '0');
 			_putchar('\n');
 		}

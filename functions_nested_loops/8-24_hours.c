cea852c552bcf5fcae28e5bc9189897f5ae6b279
Franco Cardenas <gustavo260123@gmail.com>
2022-06-10 10:29:35 -0700

    print24hoursc

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - Print the hours and minutes in a day.
+ *
+ */
+void jack_bauer(void)
+{
+	int H;
+	int m;
+
+	for (H = 0; H < 24; H++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar(H / 10) + '0');
+			_putchar(H % 10) + '0');
+			_putchar(':');
+			_putchar(m / 10) + '0)';
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}

5e7b135f5043636ec11910596b048deb4391409a
Franco Cardenas <gustavo260123@gmail.com>
2022-06-10 11:36:07 -0700

    printtableninec

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - Print the table of 9, starting with 0.
+ *
+ */
+void times_table(void)
+{
+	int Num1;
+	int Num2;
+	int Multi;
+
+	for (Num1 = 0; Num1 < 10; Num1++)
+	{
+		_putchar('0');
+		for (Num2 = 1; Num2 < 10; Num2++)
+		{
+			Multi = Num1 * Num2;
+			_putchar(44);
+			_putchar(32);
+			if (Multi < 10)
+			{
+				_putchar(32);
+				_putchar(Multi + '0');
+			}
+			else
+			{
+				_putchar((Multi / 10) + '0');
+				_putchar((Multi / 10) + '0');
+
+			}
+		}
+		_putchar('\n');
+	}
+}

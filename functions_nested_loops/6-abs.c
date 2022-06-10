438e9e26c8fbbea96b3cb27582ad92c88e12a2be
Franco Cardenas <gustavo260123@gmail.com>
2022-06-10 08:37:04 -0700

    printvalueabsolutec

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - The absolute value of an integer.
+ * @n: Character printer absolut.
+ * Return: Absolute Value.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}

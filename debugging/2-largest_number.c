8f7d336aa93208f6de6b8184a1155cdc18288907
Franco Cardenas <gustavo260123@gmail.com>
2022-06-10 14:27:34 -0700

    debbuggingtask2c

--- debugging/2-largest_number.c
+++ debugging/2-largest_number.c
@@ -1,23 +1,3 @@
-/**
-* main - prints the largest of 3 integers
-* Return: 0
-*/
-
-int main()
-{
-        int a, b, c;
-        int largest;
-
-        a = 972;
-        b = -98;
-        c = 0;
-
-        largest = largest_number(a, b, c);
-
-        printf("%d is the largest number\n", largest);
-
-        return (0);
-}
 carrie@ubuntu:/debugging$
 carrie@ubuntu:/debugging$ cat 2-largest_number.c
 #include "main.h"

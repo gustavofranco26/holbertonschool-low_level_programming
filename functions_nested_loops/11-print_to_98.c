b4eb9233b5ea8cc4d709c58ed9390e7baf8a1b3a
Franco Cardenas <gustavo260123@gmail.com>
2022-06-10 12:51:07 -0700

    printanynumber98c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98.
+ *
+ * @n: numero a utilizar
+ *
+ */
+void print_to_98(int n)
+{
+	for (n < 98; n++)
+		printf("%i\n", n);
+	{
+		if (n > 98; n--)
+			printf("%i\n", n);
+	}
+	printf("%i\n", n);
+}

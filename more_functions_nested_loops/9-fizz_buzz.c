9d415ecf952327066a1307bdbf919332187eaaac
Franco Cardenas <gustavo260123@gmail.com>
2022-06-15 15:01:46 -0700

    addtofizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - Prints the multiples 3, 5 and 15 with fizzbuzz.
+ *
+ *Description: A function that print thw fizzbuzz.
+ *Return: Always 0 (Sucess).
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else if (n % 3 && n % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", n);
+		
+		if (n >= 1 && n <= 100)
+			printf(" ");
+	}
+	return (0);
+}

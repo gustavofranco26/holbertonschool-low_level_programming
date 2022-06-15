576df8ef7430de76760a9aac9adadf21329d1ffd
Franco Cardenas <gustavo260123@gmail.com>
2022-06-15 11:07:43 -0700

    printhenumbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  *print_numbers - Prints the numbers 0 to 9.
+ *Return: Always 0.
  */
 
 void print_numbers(void)
 {
 	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		_putchar(i);
 	}

b0f380218c414cf7f0096b2d165bb9c7e66260a2
Franco Cardenas <gustavo260123@gmail.com>
2022-06-15 10:11:26 -0700

    checkthwmultiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *mul - code
+ *Description: checks that multiplies two integers.
+ *@a: Character 1
+ *@b: Character 2
+ *Return: Result of a*b.
+ */
+
+int mul(int a, int b)
+{
+	return (a*b);
+}

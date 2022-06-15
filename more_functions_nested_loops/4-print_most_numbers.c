3b28e4f912c8a6b19a7ef3eb7731f945b2edb187
Franco Cardenas <gustavo260123@gmail.com>
2022-06-15 12:27:25 -0700

    print2mostnumbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_most_numbers - Prints the numbers 2, 4 in to 0 to 9.
+ *
+ */
+
+void print_most_numbers(void);
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != '2' && i != '4')
+		_putchar(i);
+	}
+	_putchar(10);
+}

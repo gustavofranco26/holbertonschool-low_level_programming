82a7c11b9b035578e433a577ce84105f15f878d6
Franco Cardenas <gustavo260123@gmail.com>
2022-06-16 18:30:53 -0700

    addallandresetto98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main-h"
+
+/**
+ *reset_to_98 - Check updates the value it points.
+ *@n: Character at pointer
+ *Return: Always 0 (Sucess).
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

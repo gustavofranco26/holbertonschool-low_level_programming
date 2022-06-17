b6cd2a802f5e63bb354b554fbdc58d9c6c844603
Franco Cardenas <gustavo260123@gmail.com>
2022-06-17 05:43:19 -0700

    addlongString

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strlen - Check a function that returns the length of a string.
+ *@s: value character
+ *Return: Always Longcade
+ */
+int _strlen(char *s)
+{
+	int longcade;
+
+	for longcade = 0;
+
+		if (*s != '\0')
+		{
+			s++;
+			longcade++;
+		}
+	return (longcade);
+}

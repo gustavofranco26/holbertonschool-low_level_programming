4630a1232559d5b7845de89a1fdb6a736ae5ab2a
Franco Cardenas <gustavo260123@gmail.com>
2022-06-19 18:28:11 -0700

    addtoputs

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <string.h>
 
+/**
+*puts2 - check a that prints every other character of a string, starting with the first character.
+*@str: Crachacter Value
+*Return: Always (Sucess)
+*/
+
 void puts2(char *str)
 {
 	char* p = "hello world";

2611a6e305deccf029919270f06f7825ad7e7197
Franco Cardenas <gustavo260123@gmail.com>
2022-06-20 06:40:18 -0700

    addtocopystrcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i;
 
-	while (src[i])
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
+
+	dest[i] = '\0';
+	
 	return (dest);
 }

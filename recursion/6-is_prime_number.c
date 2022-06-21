0855224005fceec7b7abd2108d5125a40b889882
Franco Cardenas <gustavo260123@gmail.com>
2022-06-20 20:57:58 -0700

    addtoprimenumber

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -8,13 +8,10 @@
 
 int is_prime_number(int n)
 {
-	int i =2;
-
-	if (n <= 2)
-		return (n == 2)? true : false;
-	if (n % i == 0)
-		return false;
-	if (i * i > n)
-		return true;
-	return is_prime_number(n , i + 1);
+	if (i == 1)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+	else (i = i - 1)
+		return (is_prime_number(n));
 }

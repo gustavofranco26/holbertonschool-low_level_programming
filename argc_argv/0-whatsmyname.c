0837a550deb85b8955b0b347c821641a84144a66
Franco Cardenas <gustavo260123@gmail.com>
2022-06-23 16:37:48 -0700

    addtofilesprincipals

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Funtion name
+ *@argc: Arguments count
+ *@argv: Argument matriz
+ *Return: Always 0 (Sucess)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argv[0]);
+	return (0);
+}

2ba9e509a95d73b7da73744508ec3b4a77b24839
Franco Cardenas <gustavo260123@gmail.com>
2022-06-24 09:43:28 -0700

    addtoargsbynewline

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - Funtion name
+ *@argc: Arguments count
+ *@argv: Argument matriz
+ *Return: Always 0 (Sucess)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}

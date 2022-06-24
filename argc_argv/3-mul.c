5a371ab8fc0378e0a3db0b4dd9236fbdc4127d0b
Franco Cardenas <gustavo260123@gmail.com>
2022-06-24 10:21:59 -0700

    addtomultitwonumbers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *main - Funtion name
+ *@argc: Arguments count
+ *@argv: Argument matriz
+ *Return: Always 0 (Sucess)
+ */
+
+int main(int argc, char *argv[])
+{
+	int n1, n2, product;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	product = (n1 * n2);
+	
+	printf("%d\n", product);
+	return (0);
+}

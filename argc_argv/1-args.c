d0f2b8f026da7435216402706861ff6937d8d9f4
Franco Cardenas <gustavo260123@gmail.com>
2022-06-23 18:46:26 -0700

    addtoargsnumberarguments

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -10,11 +10,14 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[-1]);
-	}
+	//for (i = 0; i < argc; i++)
+	//{
+	//	printf("%s\n", argv[-1]);
+	//}
+	printf("%s\n", argv[0]);
+	if (argc < 2)
+		printf("\n");
+	else
+		printf("%s\n", argv[1]);
 	return (0);
 }

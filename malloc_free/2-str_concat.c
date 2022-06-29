afc0d2d6c33162111830aee1c90f2db8ebcb7dfe
Franco Cardenas <gustavo260123@gmail.com>
2022-06-28 18:58:17 -0700

    addtostrconcat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 	int mmat = 0;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 	for (i = 0; s1[i] || s2[i]; i++)
 		mmat++;
 	ptr = malloc(sizeof(char) * mmat);

3fd020f4d3303d05f3d3b01a0c5e3729d1f8ef7b
Franco Cardenas <gustavo260123@gmail.com>
2022-07-04 03:48:09 -0700

    freedogC

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ *free_dog - Write a function that frees dogs.
+ *@d: Pointerdog
+ *Return: Always
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}

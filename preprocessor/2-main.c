7de1744bc6362037eb153baf5ad8c3094aaeb2db
Franco Cardenas <gustavo260123@gmail.com>
2022-07-03 21:37:56 -0700

    addtomain

---
+++ preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Write a program that prints the name of the file it was compiled from
+ *Return: Always.
+ **/
+
+int main(void)
+{
+	printf("%d\n", __FILE__);
+	return (0);
+}

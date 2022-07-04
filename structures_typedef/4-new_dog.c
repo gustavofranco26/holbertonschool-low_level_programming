05672d92522f32e90339c318decc1b627d408979
Franco Cardenas <gustavo260123@gmail.com>
2022-07-04 03:40:14 -0700

    git addnewdogownerC

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *new_dog - Write a function that creates a new dog.
+ *@name: Name by to Validate
+ *@age: Age by to Validate
+ *@owner: Owner by to Validate
+ *Return: Always
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog = malloc(sizeof(dog_t));
+
+	if (newDog == NULL)
+		return (NULL);
+
+	newDog->name = malloc((strlen(name) + 1) * sizeof(char));
+
+	if (newDog->name == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
+
+	newDog->owner = malloc((strlen(owner) + 1) * sizeof(char));
+
+	if (newDog->owner == NULL)
+	{
+		free(newDog);
+		free(newDog->name);
+		return (NULL);
+	}
+
+	strcpy(newDog->name, name);
+	newDog->age = age;
+	strcpy(newDog->owner, owner);
+
+	return (newDog);
+}

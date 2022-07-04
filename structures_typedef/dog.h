4dca87500eccb77ecd4bae151db920a35faa20b6
Franco Cardenas <gustavo260123@gmail.com>
2022-07-04 01:02:29 -0700

    addtodogh

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Define a new type struct dog with the following elements
+ *Return: Always.
+ */
+
+int main(void)
+{
+	struct dog my_dog;
+
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	return (0);
+}

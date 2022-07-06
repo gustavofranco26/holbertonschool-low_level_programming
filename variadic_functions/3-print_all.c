458eefc18163f5d647dc9ba2bc801d2fd6298545
Franco Cardenas <gustavo260123@gmail.com>
2022-07-06 01:34:07 -0700

    addtoprintallargumentsanything

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,6 +1,11 @@
+/*
+ * File: 3-print_all.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
 
 void print_char(va_list arg);
 void print_int(va_list arg);
@@ -11,9 +16,8 @@ void print_all(const char * const format, ...);
 /**
  * print_char - Prints a char.
  * @arg: A list of arguments pointing to
- * the character to be printed.
+ *       the character to be printed.
  */
-
 void print_char(va_list arg)
 {
 	char letter;
@@ -25,9 +29,8 @@ void print_char(va_list arg)
 /**
  * print_int - Prints an int.
  * @arg: A list of arguments pointing to
- * the integer to be printed.
+ *       the integer to be printed.
  */
-
 void print_int(va_list arg)
 {
 	int num;
@@ -39,7 +42,7 @@ void print_int(va_list arg)
 /**
  * print_float - Prints a float.
  * @arg: A list of arguments pointing to
- * the float to be printed.
+ *       the float to be printed.
  */
 void print_float(va_list arg)
 {
@@ -52,7 +55,7 @@ void print_float(va_list arg)
 /**
  * print_string - Prints a string.
  * @arg: A list of arguments pointing to
- * the string to be printed.
+ *       the string to be printed.
  */
 void print_string(va_list arg)
 {
@@ -75,19 +78,19 @@ void print_string(va_list arg)
  * @...: A variable number of arguments to be printed.
  *
  * Description: Any argument not of type char, int, float,
- * or char * is ignored.
- * If a string argument is NULL, (nil) is printed instead.
+ *              or char * is ignored.
+ *              If a string argument is NULL, (nil) is printed instead.
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i = 0, j = 0;
 	char *separator = "";
-	printer_t funcs[] = 
-		"c", print_char,
-		"i", print_int,
-		"f", print_float,
-		"s", print_string
+	printer_t funcs[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
 	};
 
 	va_start(args, format);
@@ -105,8 +108,11 @@ void print_all(const char * const format, ...)
 			funcs[j].print(args);
 			separator = ", ";
 		}
+
 		i++;
 	}
+
 	printf("\n");
+
 	va_end(args);
 }

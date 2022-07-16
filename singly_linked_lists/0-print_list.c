e423c70c30934c57e0ea0886db68dcdfcc55dd1a
Franco Cardenas <gustavo260123@gmail.com>
2022-07-16 13:04:12 -0700

    linkedlistinC

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ *print_list - a function that prints all the elements of a list_t list.
+ *@h: A variable pointing to a struct of type list_t.
+ *Return: Always.
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0](nil)\n");
+		count++;
+		h = h->next;
+	}
+	return (count);
+}

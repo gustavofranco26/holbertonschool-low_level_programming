ef35698417e35e3fab57ee8eaa008111f8095cff
Franco Cardenas <gustavo260123@gmail.com>
2022-08-12 16:23:21 -0700

    (array == NULL || action == NULL)

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -9,8 +9,12 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	if (array == NULL || action == NULL)
+		return;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
 }

af794fe1e33949f76ee05756c1d020864f3a625f
Franco Cardenas <gustavo260123@gmail.com>
2022-08-12 18:48:02 -0700

    Write a function that returns the number of elements in a linked listint_t list.

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - A function that returns the number of elements
+ * 			in a linked listint_t list.
+ * @h: A pointer to the head.
+ * Return: Always.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}

a6a43cd105734e75e421a7e236dad20bfd2cc9b0
Franco Cardenas <gustavo260123@gmail.com>
2022-08-12 19:04:48 -0700

    Add the fixing in the file, type kernel.

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- *add_nodeint_end - A function that adds a new node at the end of a listint_t list.
+ *add_nodeint_end - A function that adds a new node at the end of a listint_t.
  *@head: A pointer with the adress of the head of the listint.
  *@n: The integer for the new node to contain.
  *Return: Always.

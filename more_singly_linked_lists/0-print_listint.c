ae82e03aeaa3f1739e4e9fb88f8df682dfdb776d
Franco Cardenas <gustavo260123@gmail.com>
2022-08-12 18:44:31 -0700

    Write a function that prints all the elements of a listint_t list and the fixing in the lists.h

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - A function that prints all the elements of a listint_t list.
+ * @h: A pointer to head of the list_t list.
+ * Return: Always.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (nodes);
+}

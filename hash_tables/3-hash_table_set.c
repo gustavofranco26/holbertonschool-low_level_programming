1c2127345974a9983c6c7b1da7d082c52d2f955a
Franco Cardenas <gustavo260123@gmail.com>
2022-08-21 22:00:10 -0700

    Write a function that adds an element to the hash table.

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,50 @@
+#include "hash_tables.h"
+/**
+ *hash_table_set - A function that adds an element to the hash table.
+ *@ht: Pointer to a struct hashtable 
+ *@key: key to add
+ *@value: Value of the key
+ *Return: 1 if it succeeded, 0 otherwise.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *bucket = NULL, *tmp = NULL;
+	unsigned long int hash = 0;
+
+	if (!ht || !key || !*key || !value || !*value)
+		return (0);
+	hash = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[hash];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, (char *)key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup((char *)value);
+			if (!tmp->value)
+				return (0);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	bucket = malloc(sizeof(hash_node_t));
+	if (!bucket)
+		return (0);
+	bucket->key = strdup((char *)key);
+	if (bucket->key == NULL)
+	{
+		free(bucket);
+		return (0);
+	}
+	bucket->value = strdup(value);
+	if (bucket->value == NULL)
+	{
+		free(bucket->key);
+		free(bucket);
+		return (0);
+	}
+	bucket->next = ht->array[hash];
+	ht->array[hash] = bucket;
+	return (1);
+}
